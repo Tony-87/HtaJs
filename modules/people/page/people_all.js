2cb62093676d1253a900e5f5baec61990c8a9ba8
wxb-bimt <wxb@bimt.com>
2016-08-12 08:58:01 +0800

    all

---
+++ modules/people/page/people_all.js
@@ -0,0 +1,13 @@
+define(["avalon", "text!./people_all.html"], function (avalon, people_all) {
+
+    avalon.templateCache.people_all = people_all
+
+    var model = avalon.define({
+        $id: "people_all",
+        //username: "people_all",
+        usertitle:""
+    })
+
+
+
+})
