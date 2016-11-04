992576d4cefb3d982e695bbdd9a9ef8c9017ca59
wxb-bimt <wxb@bimt.com>
2016-11-04 09:18:40 +0800

    a

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+/**
+ * Created by admin on 2016/9/22.
+ */
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('watch',function(){
+    gulp.watch('*.html',['html']);
+    gulp.watch('*/*.html',['html']);
+    gulp.watch('search/*.html',['html']);
+    gulp.watch('login/*.html',['html']);
+    gulp.watch('write/*.html',['html']);
+    gulp.watch('css/*.css',['css']);
+    gulp.watch('js/*.js',['js']);
+})
+
+gulp.task('connect',function(){
+    connect.server({
+        root:'./',
+        ip:'192.168.31.110',
+        livereload:true
+    })
+})
+
+gulp.task('html', function(){
+    gulp.src('*.html').pipe(connect.reload());
+    gulp.src('*/*.html').pipe(connect.reload());
+    gulp.src('search/*.html').pipe(connect.reload());
+    gulp.src('login/*.html').pipe(connect.reload());
+    gulp.src('write/*.html').pipe(connect.reload());
+});
+
+gulp.task('css', function(){
+    gulp.src('css/*.css').pipe(connect.reload());
+});
+
+
+gulp.task('js', function(){
+    gulp.src('js/*.js').pipe(connect.reload());
+});
+
+gulp.task('default',['connect','watch'])
